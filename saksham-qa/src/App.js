219b487aee9d060318b78a63ba823dac5501b2a2
Saksham Chand <Sakshamchand3@gmail.com>
2025-07-12 12:00:05 +0530

    Add responsive Question Detail screen. Updates codes are present in Components folder. Updates App.js

---
+++ saksham-qa/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import QuestionDetail from './components/QuestionDetail';
+
+function App() {
+  return (
+    <div className="App">
+      <QuestionDetail />
+    </div>
+  );
+}
+
+export default App;
